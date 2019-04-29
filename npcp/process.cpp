fdc724cad2b74b97cf8e8a85c6315166b7aef164
mu001999 <mu001999@outlook.com>
2019-04-29 16:27:56 +0800

    update actoins on message

--- npcp/process.cpp
+++ npcp/process.cpp
@@ -1,26 +1,26 @@
-#include <ctime>
-
 #include <mutex>
 #include <string>
 #include <unordered_map>
 
-#include "message.hpp"
-#include "process.hpp"
 #include "../icarus/icarus/buffer.hpp"
 #include "../icarus/icarus/tcpconnection.hpp"
+
+#include "message.hpp"
+#include "process.hpp"
+#include "rplfuncs.hpp"
 
 using namespace icarus;
 
 namespace
 {
-static const std::string _hostname = ":jusot.com";
+static const std::string _hostname = "jusot.com";
 
-constexpr std::size_t cal_hash(const char* str)
+constexpr std::size_t cal_hash(const char *str)
 {
     return (*str == '\0') ? 0 : ((cal_hash(str + 1) * 201314 + *str) % 5201314);
 }
 
-constexpr std::size_t operator""_hash(const char* str, std::size_t)
+constexpr std::size_t operator""_hash(const char *str, std::size_t)
 {
     return cal_hash(str);
 }
@@ -34,7 +34,7 @@ std::unordered_map<std::string, std::vector<std::string>> channels;
 
 std::mutex nick_conn_mutex;
 
-static bool _check_registered(const TcpConnectionPtr& conn)
+static bool _check_registered(const TcpConnectionPtr &conn)
 {
     return conn_session.count(conn) && nick_conn.count(conn_session[conn].nickname);
 }
@@ -43,36 +43,34 @@ static void _ins_nick_process(const TcpConnectionPtr&, const std::string&, const
 static bool _ins_user_process(const TcpConnectionPtr&, const std::string&, const std::string&, const std::vector<std::string>&);
 static bool _ins_quit_process(const TcpConnectionPtr&, const std::string&, const std::string&, const std::vector<std::string>&);
 
-void on_message(const TcpConnectionPtr& conn, Buffer* buf)
+void on_message(const TcpConnectionPtr &conn, Buffer *buf)
 {
     Message message(buf->retrieve_all_as_string());
 
-    const auto& source = message.source(), ins = message.ins();
+    const auto& source = message.source(), command = message.command();
     const auto& args = message.args();
 
-    auto hs = cal_hash(ins.c_str());
+    auto hs = cal_hash(command.c_str());
 
     switch (hs)
     {
     case "NICK"_hash:
-        _ins_nick_process(conn, source, ins, args);
+        _ins_nick_process(conn, source, command, args);
         break;
 
     case "USER"_hash:
-        _ins_user_process(conn, source, ins, args);
+        _ins_user_process(conn, source, command, args);
         break;
 
 #define RPL_WHEN_NOTREGISTERED if (!_check_registered(conn)) \ 
         { \
-            conn->send(Reply::gen_reply({_hostname, \
-                "451", \
-                ":You have not registered"})); \
+            conn->send(Reply::err_notregistered()); \
             break; \
-        }   // 451 for ERR_NOTREGISTERED
+        }
 
     case "QUIT"_hash:
         RPL_WHEN_NOTREGISTERED;
-        _ins_quit_process(conn, source, ins, args);
+        _ins_quit_process(conn, source, command, args);
         break;
 
     case "WHOIS"_hash:
@@ -85,36 +83,22 @@ void on_message(const TcpConnectionPtr& conn, Buffer* buf)
 
     default:
         if (_check_registered(conn))
-        {
-            conn->send(Reply::gen_reply({_hostname,
-                "421",  // 421 for ERR_UNKNOWNCOMMAND
-                ins,    // <command>
-                ":Unknown command"}));
-        }
+            conn->send(Reply::err_unknowncommand(command));
         break;
     }
 }
 
-static void _ins_nick_process(const TcpConnectionPtr& conn,
-    const std::string& source,
-    const std::string& ins,
-    const std::vector<std::string>& args)
+static void _ins_nick_process(const TcpConnectionPtr &conn,
+    const std::string &source,
+    const std::string &command,
+    const std::vector<std::string> &args)
 {
     std::lock_guard lock(nick_conn_mutex);
 
     if (args.empty())
-    {
-        conn->send(Reply::gen_reply({_hostname,
-            "431",  // 431 for ERR_NONICKNAMEGIVEN
-            ":No nickname given"}));
-    }
+        conn->send(Reply::err_nonicknamegiven());
     else if (nick_conn.count(args.front()))
-    {
-        conn->send(Reply::gen_reply({_hostname,
-            "432",  // 432 for ERR_NICKNAMEINUSE
-            args.front(), // <nick>
-            ":Nickname is already in use"}));
-    }
+        conn->send(Reply::err_nicknameinuse(args.front()));
     else if (conn_session.count(conn))
     {
         if (nick_conn.count(conn_session[conn].nickname))
@@ -122,68 +106,42 @@ static void _ins_nick_process(const TcpConnectionPtr& conn,
         nick_conn[args.front()] = conn;
         conn_session[conn] = { args.front(), conn_session[conn].realname };
 
-        conn->send(Reply::gen_reply({_hostname,
-            "001",  // 001 for RPL_WELCOME
-            ":Welcome to the Internet Relay Network\n" +
-            source.substr(1)}));    // <nick>!<user>@<host>
-
-        conn->send(Reply::gen_reply({_hostname,
-            "002",  // 002 for RPL_YOURHOST
-            ":Your host is " +
-            _hostname.substr(1) +       // <server name>
-            ", running version 2"}));   // <ver>
-
-        auto t = std::time(nullptr);
-        conn->send(Reply::gen_reply({_hostname,
-            "003",
-            ":This server was created " + 
-            std::string(std::ctime(&t))})); // <date>
-
-        // RETURN RPL_MYINFO
+        conn->send(Reply::rpl_welcome(source));
+        conn->send(Reply::rpl_yourhost("2"));
+        conn->send(Reply::rpl_created());
+        conn->send(Reply::rpl_myinfo("2", "ao", "mtov"));
     }
-    else
-    {
-        nick_conn[args.front()] = nullptr;
-    }
+    else nick_conn[args.front()] = nullptr;
 }
 
-static bool _ins_user_process(const TcpConnectionPtr& conn,
-    const std::string& source,
-    const std::string& ins,
-    const std::vector<std::string>& args)
+static bool _ins_user_process(const TcpConnectionPtr &conn,
+    const std::string &source,
+    const std::string &command,
+    const std::vector<std::string> &args)
 {
     std::lock_guard lock(nick_conn_mutex);
 
     if (conn_session.count(conn))
-    {
-        conn->send(Reply::gen_reply({_hostname,
-            "462",  // 462 for ERR_ALREADYREGISTERED
-            ":Unauthorized command (already registered)"}));
-    }
+        conn->send(Reply::err_alreadyregistered());
     else if (args.size() != 4)
-    {
-        conn->send(Reply::gen_reply({_hostname,
-            "461",  // 461 for ERR_NEEDMOREPARAMS
-            ins,    // <command>
-            ":Not enough parameters"}));
-    }
+        conn->send(Reply::err_needmoreparams(command));
     else
     {
         conn_session[conn] = { args[0], args[3] };
         if (nick_conn.count(args[0]))
         {
-            // RETURN RPL_WELCOME
-            // RETURN RPL_YOURHOST
-            // RETURN RPL_CREATED
-            // RETURN RPL_MYINFO
+            conn->send(Reply::rpl_welcome(source));
+            conn->send(Reply::rpl_yourhost("2"));
+            conn->send(Reply::rpl_created());
+            conn->send(Reply::rpl_myinfo("2", "ao", "mtov"));
         }
     }
 }
 
-static bool _ins_quit_process(const TcpConnectionPtr& conn,
-    const std::string& source,
-    const std::string& ins,
-    const std::vector<std::string>& args)
+static bool _ins_quit_process(const TcpConnectionPtr &conn,
+    const std::string &source,
+    const std::string &command,
+    const std::vector<std::string> &args)
 {
     std::lock_guard lock(nick_conn_mutex);
 
@@ -192,5 +150,4 @@ static bool _ins_quit_process(const TcpConnectionPtr& conn,
 
     // RETURN Closing Link: HOSTNAME (MSG)
 }
-
 }
