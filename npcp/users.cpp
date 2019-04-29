e054c1e61651468a8b218b25e24284a8e8e6113e
mu001999 <mu001999@outlook.com>
2019-04-29 11:28:21 +0800

    update on_message

--- npcp/users.cpp
+++ npcp/users.cpp
@@ -27,83 +27,15 @@ std::unordered_map<std::string, TcpConnectionPtr> nick_conn;
 std::unordered_map<TcpConnectionPtr, Session> conn_session;
 
 std::mutex nick_conn_mutex;
-std::mutex conn_session_mutex;
 
 static bool _check_registered(const TcpConnectionPtr &conn)
 {
     return conn_session.count(conn) && nick_conn.count(conn_session[conn].nickname);
 }
 
-static void _ins_nick_process(const TcpConnectionPtr &conn,
-    const std::string &source,
-    const std::string &ins,
-    const std::vector<std::string> &args)
-{
-    if (args.empty())
-    {
-        // RETURN ERR_NONNICKNAMEGIVEN
-    }
-    else if (nick_conn.count(args.front()))
-    {
-        // RETURN ERR_NICKNAMEINUSE
-    }
-    else if (conn_session.count(conn))
-    {
-        {
-            std::lock_guard lock(nick_conn_mutex);
-            if (nick_conn.count(conn_session[conn].nickname))
-                nick_conn.erase(conn_session[conn].nickname);
-            nick_conn[args.front()] = conn;
-        }
-
-        {
-            std::lock_guard lock(conn_session_mutex);
-            conn_session[conn] = { args.front(), conn_session[conn].realname };
-        }
-
-        // RETURN RPL_WELCOME
-        // RETURN RPL_YOURHOST
-        // RETURN RPL_CREATED
-        // RETURN RPL_MYINFO
-    }
-    else
-    {
-        {
-            std::lock_guard lock(nick_conn_mutex);
-            nick_conn[args.front()] = nullptr;
-        }
-    }
-}
-
-static bool _ins_user_process(const TcpConnectionPtr& conn,
-    const std::string& source,
-    const std::string& ins,
-    const std::vector<std::string>& args)
-{
-    if (conn_session.count(conn))
-    {
-        // RETURN ERR_ALREADYREGISTRED
-    }
-    else if (args.size() != 4)
-    {
-        // RETURN ERR_NEEDMOREPARAMS
-    }
-    else
-    {
-        {
-            std::lock_guard lock(conn_session_mutex);
-            conn_session[conn] = { args[0], args[3] };
-        }
-
-        if (nick_conn.count(args[0]))
-        {
-            // RETURN RPL_WELCOME
-            // RETURN RPL_YOURHOST
-            // RETURN RPL_CREATED
-            // RETURN RPL_MYINFO
-        }
-    }
-}
+static void _ins_nick_process(const TcpConnectionPtr&, const std::string&, const std::string&, const std::vector<std::string>&);
+static bool _ins_user_process(const TcpConnectionPtr&, const std::string&, const std::string&, const std::vector<std::string>&);
+static bool _ins_quit_process(const TcpConnectionPtr&, const std::string&, const std::string&, const std::vector<std::string>&);
 
 void on_message(const TcpConnectionPtr& conn, Buffer* buf)
 {
@@ -124,26 +56,109 @@ void on_message(const TcpConnectionPtr& conn, Buffer* buf)
         _ins_user_process(conn, source, ins, args);
         break;
 
-    default:
-        if (!check_registered(conn))
+    case "QUIT"_hash:
+        if (!_check_registered(conn))
         {
             // RETURN ERR_NOTREGISTERED
         }
-        else switch (hs)
-        {
-        case "QUIT"_hash:
-            break;
+        _ins_quit_process(conn, source, ins, args);
+        break;
         
-        case "WHOIS"_hash:
-            break;
+    case "WHOIS"_hash:
+        if (!_check_registered(conn))
+        {
+            // RETURN ERR_NOTREGISTERED
+        }
+        break;
 
-        case "MODE"_hash:
-            break;
+    case "MODE"_hash:
+        if (!_check_registered(conn))
+        {
+            // RETURN ERR_NOTREGISTERED
+        }
+        break;
 
-        default:
-            break;
+    default:
+        if (_check_registered(conn))
+        {
+            // RETURN ERR_UNKNOWNCOMMAND
         }
         break;
     }
 }
+
+static void _ins_nick_process(const TcpConnectionPtr &conn,
+    const std::string &source,
+    const std::string &ins,
+    const std::vector<std::string> &args)
+{
+    std::lock_guard lock(nick_conn_mutex);
+
+    if (args.empty())
+    {
+        // RETURN ERR_NONNICKNAMEGIVEN
+    }
+    else if (nick_conn.count(args.front()))
+    {
+        // RETURN ERR_NICKNAMEINUSE
+    }
+    else if (conn_session.count(conn))
+    {
+        if (nick_conn.count(conn_session[conn].nickname))
+            nick_conn.erase(conn_session[conn].nickname);
+        nick_conn[args.front()] = conn;
+        conn_session[conn] = { args.front(), conn_session[conn].realname };
+
+        // RETURN RPL_WELCOME
+        // RETURN RPL_YOURHOST
+        // RETURN RPL_CREATED
+        // RETURN RPL_MYINFO
+    }
+    else
+    {
+        nick_conn[args.front()] = nullptr;
+    }
+}
+
+static bool _ins_user_process(const TcpConnectionPtr &conn,
+    const std::string &source,
+    const std::string &ins,
+    const std::vector<std::string> &args)
+{
+    std::lock_guard lock(nick_conn_mutex);
+
+    if (conn_session.count(conn))
+    {
+        // RETURN ERR_ALREADYREGISTRED
+    }
+    else if (args.size() != 4)
+    {
+        // RETURN ERR_NEEDMOREPARAMS
+    }
+    else
+    {
+        conn_session[conn] = { args[0], args[3] };
+        if (nick_conn.count(args[0]))
+        {
+            // RETURN RPL_WELCOME
+            // RETURN RPL_YOURHOST
+            // RETURN RPL_CREATED
+            // RETURN RPL_MYINFO
+        }
+    }
+}
+
+static bool _ins_quit_process(const TcpConnectionPtr& conn,
+    const std::string& source,
+    const std::string& ins,
+    const std::vector<std::string>& args)
+{
+    std::lock_guard lock(nick_conn_mutex);
+
+    nick_conn.erase(conn_session[conn].nickname);
+    conn_session.erase(conn);
+
+    // RETURN Closing Link: HOSTNAME (MSG)
+}
+
 }
