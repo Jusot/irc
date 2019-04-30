4df4f3353b518c5c4d2cb9dfd16d5fa09529e6ee
mu001999 <mu001999@outlook.com>
2019-04-30 16:50:12 +0800

    fix some written bug

--- npcp/process.hpp
+++ npcp/process.hpp
@@ -21,11 +21,11 @@ class Process
 private:
     static bool check_registered(const TcpConnectionPtr&);
 
-    static void Process::nick_process(const TcpConnectionPtr&, const Message&);
-    static void Process::user_process(const TcpConnectionPtr&, const Message&);
-    static void Process::quit_process(const TcpConnectionPtr&, const Message&);
-    static void Process::privmsg_process(const TcpConnectionPtr&, const Message&);
-    static void Process::ping_process(const TcpConnectionPtr&, const Message&);
+    static void nick_process(const TcpConnectionPtr&, const Message&);
+    static void user_process(const TcpConnectionPtr&, const Message&);
+    static void quit_process(const TcpConnectionPtr&, const Message&);
+    static void privmsg_process(const TcpConnectionPtr&, const Message&);
+    static void ping_process(const TcpConnectionPtr&, const Message&);
 
 public:
     static void on_message(const icarus::TcpConnectionPtr& conn, icarus::Buffer* buf /*, Timestamp*/);
